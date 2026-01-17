0092a8a72767c8b7e742f5b9bdc08d5a181c059d
Bind Vivek Kumar <155793749+BindVivekKumar@users.noreply.github.com>
2026-01-17 08:55:32 +0530

    Update tenant.js

--- src/Bothfeatures/features2/api/tenant.js
+++ src/Bothfeatures/features2/api/tenant.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const USER_API = "http://localhost:5000/api/tenant/";
+const USER_API = "https://roomgi-backend-project-1.onrender.com/api/tenant/";
 
 
 const TenantApi = createApi({
