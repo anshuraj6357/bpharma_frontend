26aa20d414519c98d31c6007d038cee72a610472
Bind Vivek Kumar <155793749+BindVivekKumar@users.noreply.github.com>
2026-01-17 08:55:15 +0530

    Update staffapi.js

--- src/Bothfeatures/features2/api/staffapi.js
+++ src/Bothfeatures/features2/api/staffapi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const staffApi = createApi({
     reducerPath: "staffApi",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:5000/api/staff", // adjust if needed
+        baseUrl: "https://roomgi-backend-project-1.onrender.com/api/staff", // adjust if needed
         credentials: "include",
     }),
     tagTypes: ["Staff"],
