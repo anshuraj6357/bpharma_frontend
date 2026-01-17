e3e46d7c7a1a3f857fd4a29972232f3b91d623a5
Abhinav Kumar <kumabhi139@gmail.com>
2026-01-17 15:56:05 +0530

    updated

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyB7ivr8Ekt2FXW4xEXABm-pY1atVSQBjWM",
   authDomain: "roomgi.firebaseapp.com",
   projectId: "roomgi",
-  storageBucket: "roomgi.firebasestorage.app",
+  storageBucket: "roomgi.firebasemstorage.app",
   messagingSenderId: "15819721748",
   appId: "1:15819721748:web:b2fcfbc82bd1222b88eb01",
 };
